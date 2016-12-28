ef674484ecc7f8498ee5a88525a587533706a84a
CheHsuan <exas60807@gmail.com>
2016-12-28 17:03:12 +0800

    change optimizer

--- pong/rl-pong.py
+++ pong/rl-pong.py
@@ -12,7 +12,6 @@ def policy_network():
         # Define input and output dimension.
         observation = tf.placeholder(tf.float32, shape=(None, input_dim))
         action = tf.placeholder(tf.float32, shape=(None, output_dim))
-        reward = tf.placeholder(tf.float32)
 
         # Variables.
         w1 = tf.Variable(tf.truncated_normal([input_dim, hidden_dim]))
@@ -24,13 +23,12 @@ def policy_network():
         hidden_layer = tf.nn.relu(tf.matmul(observation, w1) + b1)
         output_layer = tf.matmul(hidden_layer, w2) + b2
         probability = tf.nn.softmax(output_layer)
-        goal = tf.mul(action, reward)
 
         # Optimizer.
-        loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(probability,goal))
-        policy_optimizer = tf.train.AdamOptimizer(0.0001).minimize(loss)
+        loss= tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(probability, action))
+        policy_optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
-        return observation, action, reward, probability, policy_optimizer
+        return observation, action, probability, policy_optimizer
 
 class Pong:
     def __init__(self): 
@@ -38,7 +36,7 @@ class Pong:
         self.output_dim = 2
         self.gamma = 0.9
         self.env = gym.make('Pong-v0')
-        self.tf_observation, self.tf_action, self.tf_reward, self.tf_probability, self.tf_policy_optimizer = policy_network()
+        self.tf_observation, self.tf_action, self.tf_probability, self.tf_policy_optimizer = policy_network()
         self.sess = tf.InteractiveSession()
         self.sess.run(tf.initialize_all_variables()) 
 
@@ -59,8 +57,8 @@ class Pong:
         new_state = state.astype(np.float).ravel()
         return new_state
 
-    def training(self, state_seq, action_seq, reward):
-        feed_dict = {self.tf_observation : state_seq, self.tf_action : action_seq, self.tf_reward : reward}
+    def training(self, state_seq, action_seq):
+        feed_dict = {self.tf_observation : state_seq[1:], self.tf_action : action_seq[1:]}
         self.sess.run(self.tf_policy_optimizer, feed_dict=feed_dict)
 
     def run(self, game):
@@ -76,24 +74,25 @@ class Pong:
                 cur_state = self.encode_state(observation)
                 diff = cur_state - prev_state if prev_state is not None else np.zeros(self.input_dim)
                 prev_state = cur_state
-                state_seq.append(diff)
+                
                 action, action_row = self.choose_action(diff)
                 observation, reward, done, info = self.env.step(action)
-                action_seq.append(action_row * discount_factor)
+                action_seq.append(action_row)
+                state_seq.append(diff)
+                
                 discount_factor *= self.gamma
                 if reward == 1:
-                    # get a score
-                    self.training(state_seq, action_seq, reward)
+                    # player gets a score
+                    self.training(state_seq, action_seq)
                     state_seq = []
                     action_seq = []
                     discount_factor = 1
                     prev_state = None
                     score[1] += 1
                 elif reward == -1:
-                    # > 50 means that at least one bounce occured 
+                    # > 50 means that at least one bounce occured
                     if len(state_seq) > 50:
-                        reward = 1
-                        self.training(state_seq, action_seq, reward)
+                        self.training(state_seq, action_seq)
                     state_seq = []
                     action_seq = []
                     discount_factor = 1
